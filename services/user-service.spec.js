003e067ff325f660255e8145158fed2749bdd873
KHELIFI Alaeddine (CONTRACTOR) <AKI18@dsone.3ds.com>
2021-01-20 15:52:39 +0100

    ENH: Test for getUserSubscription() function

--- services/user-service.spec.js
+++ services/user-service.spec.js
@@ -4,5 +4,9 @@ describe('UserService', () => {
   test('getUsers() returns all users', () => {
     expect(userService.getUsers()).toBeDefined();
     expect(userService.getUsers().length).toBe(10);
-  })
+  });
+  test('getUserSubscription() returns correct values for user subscription', () => {
+    expect(userService.getUserSubscription(5)).toBeDefined();
+    expect(userService.getUserSubscription(4)).toBeUndefined();
+  });
 });
