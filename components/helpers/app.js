0ada0908828aedc28b1c5e62177c2f45422655c0
KHELIFI Alaeddine (CONTRACTOR) <AKI18@dsone.3ds.com>
2021-01-20 14:06:33 +0100

    ENH: Etape 2

--- components/helpers/app.js
+++ components/helpers/app.js
@@ -1,6 +1,6 @@
 //const userService = require('../../services/user-service');
 const hotelService = require('../../services/hotel-service');
-//const priceService = require('../../services/price-service');
+const priceService = require('../../services/price-service');
 const helper = require('../../services/helper');
 const _ = require('lodash');
 
@@ -15,18 +15,50 @@ function findHotelsNearby(lat, lng, radius) {
     return nearbyHotels
 }
 
-/*function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
-    // TODO implement me
-    return null;
+function SortOffers(offers) {
+    
+    offers = _.remove(offers, function(item) { 
+        return item.offer !== undefined; 
+    });
+
+    offers.sort((a,b) => (a.offer.price - b.offer.price))
+
+    let cheapest = null
+
+    if(offers[0].offer.price != offers[1].offer.price){
+        cheapest = offers[0]
+    } else {
+        let samePrice = [offers[0]]
+        for(let i = 0; i<offers.length-1; i++){
+            if(offers[i].offer.price === offers[i+1].offer.price){
+                samePrice.push(offers[i+1])
+            } else {
+                break;
+            }
+        }
+        cheapest = _.minBy(samePrice, function(item) { return item.distance });
+    }
+    return cheapest
 }
 
-function findHotelNearbyWithBestOfferForUser(lat, lng, radius, date, userId) {
+function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
+    if(lat && lng && radius && date){
+        const hotels = findHotelsNearby(lat, lng, radius)
+        let allOffers = priceService.getSpecificPrices(hotels, date, 'STANDARD')
+        return SortOffers(allOffers)
+    } else {
+        return null
+    }
+    
+}
+
+/*function findHotelNearbyWithBestOfferForUser(lat, lng, radius, date, userId) {
     // TODO implement me
     return null;
 }*/
 
 module.exports = {
 	findHotelsNearby: findHotelsNearby,
-	/*findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer,
-	findHotelNearbyWithBestOfferForUser: findHotelNearbyWithBestOfferForUser*/
+	findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer,
+	/*findHotelNearbyWithBestOfferForUser: findHotelNearbyWithBestOfferForUser*/
 }
--- services/price-service.js
+++ services/price-service.js
@@ -1,9 +1,23 @@
-const PRICES = require('./data/prices.json').prices;
+let PRICES = require('./data/prices.json').prices;
+const _ = require('lodash');
 
 const getPrices = () => {
 	return PRICES;
 }
 
+const getSpecificPrices = (hotels, date, offerType) => {
+	
+	let allHotelOffers = _.forEach(hotels, function(hotel) {
+		hotel.offer = PRICES.filter(item => item.ridCode === hotel.ridCode);
+	});
+
+	const specificPrices = _.forEach(allHotelOffers, function(hotelOffer) {
+		hotelOffer.offer = hotelOffer.offer[0].offers.filter(item => item.fare === offerType && item.date === date)[0];
+	});
+
+	return specificPrices
+}
 module.exports = {
+	getSpecificPrices: getSpecificPrices,
 	getPrices: getPrices
 }
